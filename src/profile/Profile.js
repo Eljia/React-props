07b957ebd4d4d81bfbdd42905e2ec77a7a17e0f6
eljia <benaissailjia@gmail.com>
2021-09-16 18:39:50 +0100

    profil

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,17 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-const Profile = ({ props, adresse }) => {
+const Profile = (props) => {
   const handleName = (fullName) => alert(`this is profil fullname ${fullName}`);
 
   return (
     <div>
-      adresse :{adresse}
-      <h1>{props.profil.fullName}</h1>
-      <p>{props.profil.bio}</p>
-      <span>{props.profil.profession}</span>
+      <h1>{props.proo.fullName}</h1>
+      <h4>{props.proo.profession}</h4>
       <div>{props.children}</div>
-      <button onClick={() => handleName(props.profil.fullName)}></button>
+      <p className="bio">{props.proo.bio}</p>
+
+      <button onClick={() => handleName(props.proo.fullName)}> click</button>
     </div>
   );
 };
